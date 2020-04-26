37604dd2c6af2f203ce5ac9c209f2ab689b4f01d
Alexey <limendger@gmail.com>
2020-04-26 17:21:55 +0300

    * Fix.

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,6 @@ import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from '../globalStyle';
 import { defaultTheme } from '../ui';
 
-import 'swiper/css/swiper.css'
-
 export default class MyApp extends App {
     render() {
         const {Component, pageProps} = this.props
